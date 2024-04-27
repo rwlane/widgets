54a694e49400c1b45743e96c9d124b4130d10f6a
rwlane999 <rwlane999@gmail.com>
2024-04-27 14:43:46 +0100

    widgets-1, added typing to package using vite-plugin-dts.

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths()
+    tsconfigPaths(),
+    dts({rollupTypes: true})
   ],
   build: {
     lib: {
